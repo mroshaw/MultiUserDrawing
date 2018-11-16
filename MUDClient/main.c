88d50a141128c5aef636b5d33027a5939777d91d
Iain Ollerenshaw <oli@mroshaw.co.uk>
2018-11-16 15:48:07 +0000

    Added batch files to run in bash

--- MUDClient/main.c
+++ MUDClient/main.c
@@ -24,9 +24,9 @@ int main(int argc, char *argv[])
 {
 	Tcl_Interp *myinterp;
 	int error;
-	char *winWorkingDir;
 	char workingDir[PATH_MAX];
-	char *tclWorkingDir;
+	char *cygPath;
+	char *tclInitPath;
 	char const *errorString;
 
 	/* Create our Tcl interpreter */
@@ -42,24 +42,21 @@ int main(int argc, char *argv[])
 
 	/* Source the init.tcl file and we are done!	*/
     if (getcwd(workingDir, sizeof(workingDir)) != NULL) {
-        fprintf(stderr, "Current UN*X working directory: %s\n", workingDir);
+        cygPath = str_replace(workingDir, "/cygdrive/c", "C:");
+        fprintf(stderr, "Working directory: %s\n", cygPath);
     }
     else
     {
         fprintf(stderr, "Failed to get working directory. Using root . ");
-        strcpy(workingDir, ".");
+        cygPath = ".";
     }
 
-    /* Get the Windows working DIR */
-    winWorkingDir = str_replace(workingDir, "/cygdrive/c", "C:");
-    fprintf(stderr, "Current WIN working directory: %s\n", winWorkingDir);
-
     /* Get Tcl dir */
-    tclWorkingDir = strcat(winWorkingDir, "/Tcl/init.tcl");
-    fprintf(stderr, "TCL working directory: %s\n", tclWorkingDir);
+    tclInitPath = strcat(cygPath, "/Tcl/init.tcl");
+    fprintf(stderr, "TCL init path: %s\n", tclInitPath);
 
 	// error = Tcl_EvalFile(myinterp, "C:\\MUD\\MUDClient\\Tcl\\init.tcl");
-    error = Tcl_EvalFile(myinterp, tclWorkingDir);
+    error = Tcl_EvalFile(myinterp, tclInitPath);
 
     /* If we get a Tcl parse error, display the detail and fail nicely */
 	if (error==TCL_ERROR)
